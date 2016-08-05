64ebfa9ad9ed6ec7172275d6fe2f761374e85370
Karen Langner <krenabeth@yahoo.com>
2016-08-05 15:56:37 -0400

    tiny changes

--- public/js/controllers/recommendationsCtrl.js
+++ public/js/controllers/recommendationsCtrl.js
@@ -1,1 +1,18 @@
 var app = angular.module('fitnessApp');
+
+
+app.controller('aboutCtrl', function($scope) {
+	$scope.recommendations = [
+			{
+				source: "Mayo Clinic",
+				subject: "High blood pressure",
+				preview: " ",
+				path: "http://www.mayoclinic.org/diseases-conditions/high-blood-pressure/basics/definition/con-20019580",
+
+			},
+			{
+
+			},
+
+	]
+});
