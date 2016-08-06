ca10695fbb0c3e8400deef1221a0e4bb3b59470b
Karen Langner <krenabeth@yahoo.com>
2016-08-06 16:46:15 -0400

    added friend to test

--- public/js/controllers/friendscontroller.js
+++ public/js/controllers/friendscontroller.js
@@ -5,6 +5,17 @@ app.controller('friendsController', ["$scope", function($scope) {
 // search for friend- typing friend's name sorts and brings friend to top of list, submit takes to friends page(?) if match
 $scope.findFriend = function() {
 	// code goes here
+	$scope.friends = [
+		{
+		name: "John",
+		imagePath: "http://i720.photobucket.com/albums/ww206/skim4life21/sillydog.jpg",
+		altText: "John's game face",
+		info: "John chased his tail 72,000 steps today.",
+		profilePath: "http://www.funnydogsite.com/"	
+		}
+	];
+
+
 };
 
 // delete button removes friend from friends list
