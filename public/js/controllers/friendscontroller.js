018086f13b024e95dea8c132a6964c8c09abddfa
Karen Langner <krenabeth@yahoo.com>
2016-08-06 18:08:56 -0400

    working on deleting selected friend card

--- public/js/controllers/friendscontroller.js
+++ public/js/controllers/friendscontroller.js
@@ -2,26 +2,38 @@ var app = angular.module('fitnessApp');
 
 app.controller('friendsController', ["$scope", function($scope) {
 
+	$scope.friends = [
+		{
+			name: "Samantha",
+			imagePath: "http://www.becauseimacat.com/wp-content/uploads/2015/11/Cats-Gone-Crazy-22.jpg",
+			altText: "Samantha enjoys karate",
+			info: "Samantha's heart rate is 100 bpm",
+			profilePath: "http://thefw.com/karate-cats/"	
+		},
+		{
+			name: "John",
+			imagePath: "http://i720.photobucket.com/albums/ww206/skim4life21/sillydog.jpg",
+			altText: "John's game face",
+			info: "John chased his tail 72,000 times today.",
+			profilePath: "http://www.funnydogsite.com/"	
+		}
+	];
 // search for friend- typing friend's name sorts and brings friend to top of list, submit takes to friends page(?) if match
 $scope.findFriend = function() {
 	// code goes here
-	$scope.friends = [
-		{
-		name: "John",
-		imagePath: "http://i720.photobucket.com/albums/ww206/skim4life21/sillydog.jpg",
-		altText: "John's game face",
-		info: "John chased his tail 72,000 steps today.",
-		profilePath: "http://www.funnydogsite.com/"	
-		}
-	];
-
-
 };
 
 // delete button removes friend from friends list
+
 $scope.deleteFriend = function() {
-	$scope.friends.shift($scope.friend);
+	// deletes first friend? want it to delete selected
+	$scope.friends.shift($scope.friend);   
 };
 
+// this is still not deleting the selected friend
+// $scope.deleteFriend = function(item) {
+//   var index = $scope.friends.indexOf(item);
+//   $scope.friends.splice(index, 1);  
+// };
 
 }]);
