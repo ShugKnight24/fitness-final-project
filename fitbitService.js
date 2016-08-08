74200944e1c0546b51f3063642bbed20c777a520
Syed Rashid <rashidindigo@gmail.com>
2016-08-08 18:14:04 -0400

    Add script to capture access token

--- fitbitService.js
+++ fitbitService.js
@@ -1,11 +1,17 @@
-// Simple GET request example:
-$http({
-  method: 'GET',
-  url: '/someUrl'
-}).then(function successCallback(response) {
-    // this callback will be called asynchronously
-    // when the response is available
-  }, function errorCallback(response) {
-    // called asynchronously if an error occurs
-    // or server returns response with an error status.
-  });
+angular.module('fitnessApp')
+
+// .service('accessTokenService', function($http) {
+//   this.sendRequest = function(callback) {
+//     $http({
+//       method: 'POST',
+//       url: '/someUrl',
+//       Authorization: 
+//     }).then(function successCallback(response) {
+//         // this callback will be called asynchronously
+//         // when the response is available
+//       }, function errorCallback(response) {
+//         // called asynchronously if an error occurs
+//         // or server returns response with an error status.
+//       });
+//       };
+// });
