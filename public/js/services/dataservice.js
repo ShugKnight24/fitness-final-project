df806696b1e027b993d0ffc73849286180b34eae
Syed Rashid <rashidindigo@gmail.com>
2016-08-09 13:22:00 -0400

    Add fitbit data request

--- public/js/services/dataservice.js
+++ public/js/services/dataservice.js
@@ -1,9 +1,35 @@
 var app = angular.module('fitnessApp');
 
 app.service('dataService', function($http) {
-	this.getData = function(callback) {
-		$http.get('mock/shugdata.json')
-		// keeping our options open
-			.then(callback);
+ // this.getData = function(callback) {
+ // 	$http({
+ // 	  method: 'GET',
+ // 	  url: 'https://api.fitbit.com/1/user/'+ fragment['user_id'] + '/profile.json',
+ // 		Authorization: fragment['token_type'] + fragment['access_token']
+ // 	}).then(function successCallback(response) {
+ // 		return callback();
+ // 	}, function errorCallback(response) {
+ // 		console.log(response.data);
+ // 	});
+ // };
+ 
+ function errorCallback() {
+	 console.log(response.statusText);
+	 $http.get('mock/shugdata.json')
+	 .then(callback);
 	};
+ 
+ var req = {
+	 method: 'GET',
+	 url: 'https://api.fitbit.com/1/user/'+ fragment['user_id'] + '/profile.json',
+	 headers: {
+		 'Authorization': 'Bearer ' + fragment.access_token
+	 }
+ }
+ 
+ this.getData = function(callback) {
+	 console.log('Bearer ' + fragment['access_token']);
+	 $http(req).then(callback, errorCallBack);
+ };
+ 
 });
