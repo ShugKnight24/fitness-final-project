319a67626af3d958e9103adc261983c599e67d60
Karen Langner <krenabeth@yahoo.com>
2016-08-11 10:13:46 -0400

    filling out info on landing about the project

--- public/js/controllers/friendscontroller.js
+++ public/js/controllers/friendscontroller.js
@@ -1,23 +1,23 @@
 var app = angular.module('fitnessApp');
 
-app.controller('friendsController', ["$scope", function($scope) {
-// fake friends
-	$scope.friends = function(user) {
-		// stuff here
-	}
+app.controller('friendsController', function($scope) {
+// app.controller('friendsController', ["$scope", "friendProfileService",
+// 	function($scope, friendProfileService) {
+// 	var myFriends = new friendProfileService();
+// get friends array from service
+	// $scope.friends = myFriends.getFriendProfiles();
 
-	
-// search for friend
-$scope.findFriend = function() {
+	// search for friend
+	$scope.findFriend = function() {
 	// code goes here
-};
-// search for friend
-$scope.compare = function() {
+	};
 	// send user to compare with friend selected
-};
-// delete button removes friend from friends list
-$scope.deleteFriend = function(friends, index){
-    friends.splice(index, 1);
-};
+	$scope.compare = function(friends, index) {
+		$location.path('compare');
+	};
+	// delete button removes friend from friends list
+	$scope.deleteFriend = function(friends, index){
+    	friends.splice(index, 1);
+	};
 
-}]);
+});
