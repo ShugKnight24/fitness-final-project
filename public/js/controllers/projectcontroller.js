9b83bd5e0615b91305fbe871f5055e66d0708fcc
Karen Langner <krenabeth@yahoo.com>
2016-08-13 23:44:36 -0400

    materializing

--- public/js/controllers/projectcontroller.js
+++ public/js/controllers/projectcontroller.js
@@ -7,7 +7,7 @@ app.controller('projectController', function($scope) {
 $scope.slides = [
     {
         title: "Loving your fitbit?",
-        content: "Unwilling to move if you're not wearing it? You can take it to the next level with FitKnit - an interactive app to increase your motivation to get and stay fit.",
+        content: "You can take it to the next level with FitKnit - an interactive app to increase your motivation to get and stay fit.",
         image: {
             path: "https://pbs.twimg.com/profile_images/684426513773531136/Tk5cdz9f.png",
             alt: "fitbit",
@@ -16,9 +16,19 @@ $scope.slides = [
     },
     {
         title: "How it Works",
-        content1: "FitKnit knits together your fitbit statistics and motivational psychology, primarily:",
-        span1: "Self-monitoring:", 
-        content:"viewing how many steps you're taking in an average day",
+        content1: "FitKnit knits together your fitbit statistics and motivational psychology.",
+        span1: "", 
+        content: "",
+        image: {
+            path: "",
+            alt: ""
+        }
+    },
+    {
+        title: "",
+        content1: "",
+        span1: "Self-monitoring: ", 
+        content:"viewing your activity",
         image: {
             path: "images/two_step_grant.jpg",
             alt: "Only two steps today? I better get moving! Grant Chirpus says."
@@ -26,8 +36,8 @@ $scope.slides = [
     },
     {
         title: "",
-        span1: "Cognitive restructuring:",
-        content: "self-talk in the style of: 'I can do this!' instead of 'wow, I'm sick of exercising, and I feel like a big blob and all my steps are between the couch and the bathroom.'",
+        span1: "Cognitive restructuring: ",
+        content: "positive self-talk",
         image: {
             path: "images/positivelyGrant.jpg",
             alt: "Grant Chirpus says, My thighs are killing me from all those stairs yesterday. Let's do it again!"
@@ -35,7 +45,7 @@ $scope.slides = [
     },
     {
         title: "",
-        span1: "Modeling:",
+        span1: "Modeling: ",
         content: "learning by watching another person perform the healthy behaviors you aspire to",
         image: {
             path: "images/like_ray_grant.jpg",
@@ -44,7 +54,7 @@ $scope.slides = [
     },
     {
         title: "",
-        span1: "Goal-setting:",
+        span1: "Goal-setting: ",
         content: "developing concrete, specific steps to take toward a goal",
         image: {
             path: "images/grants_plan.jpg",
@@ -52,8 +62,8 @@ $scope.slides = [
         }
     },
     {
-        title: "Stretch goals",
-        content: "The 'friends' in our app are pre-populated. We'd like to make this the real deal with a backend that brings in your real friends.",
+        title: "FitKnit stretch goals",
+        content: "The 'friends' in the FitKnit app are pre-populated. We'd like to make this the real deal with a backend that brings in your real friends.",
         image: {
             path: "",
             alt: ""
@@ -62,8 +72,3 @@ $scope.slides = [
 ];
 
 });
-
-
-    
-
-
